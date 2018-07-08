b5840e704838d22fdc04d888d69b9fa8eefe8508
MG <mg84@bk.ru>
2018-07-08 23:40:05 +0300

    connect to db add user schema add dish schema

--- server/server.js
+++ server/server.js
@@ -3,9 +3,40 @@ const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 const app = express();
-const dbUrl = 'mongodb://localhost/mongoose';
+const dbUrl = 'mongodb+srv://droneadmin:8APndnqKYshne9A0@cluster0-dmatc.gcp.mongodb.net/test?retryWrites=true';
 
 const port = process.env.PORT || 3000;
+
+const user = mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true
+  }
+});
+
+const dish = mongoose.Schema({
+  title: String,
+  image: String,
+  rating: Number,
+  price: Number,
+  ingredients: {
+    type: Array,
+    items: { type: 'string'},
+  }
+});
+
+const UserModel = mongoose.model(`User`, user);
+//const DishModel = mongoose.model(`Dish`, dish);
+
+const sender = (status, msg, obj) => {
+  obj.json({
+    status: status,
+    msg: msg
+  })
+};
 
 app.use(express.static(__dirname + '/../app'));
 app.use(bodyParser.json());
@@ -15,10 +46,43 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/users/', (req, res) => {
+  UserModel.find((err, users) => {
+    if (err) {
+      sender('err', err, res);
+    }
+    sender('OK', users, res);
+  })
+});
+
+app.post('/users/', (req, res) => {
+  const user = new UserModel({
+    name: req.body.name
+  });
+
+  user.save(err => {
+    if (err) {
+      sender('err', err, res)
+    }
+    sender('OK', 'User added', res);
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
   console.log(`App started on ${port} port`);
-  //mongoose.connect(dbUrl, err => {
-  //  if (err) return console.log(err);
-  //  console.log('Db connected');
-  //});
+  mongoose.connect(dbUrl, err => {
+    if (err) return console.log(err);
+    console.log('Db connected');
+  });
 });
